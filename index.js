0be4c9c65bfd43ca27f1ef9732d1299310c56e15
Josue Jaila <eusojAliaj30122002@gmail.com>
2024-07-03 14:24:06 -0500

    Estado finalizado portafolio de prueba

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    let circle = document.querySelectorAll('.circle');
+    circle.forEach(function(progress)){
+        let degree = 0;
+        var targetDegree = parseInt(progress.getAttribute('data-degree'));
+        let color = progress.getAttribute('data-color')
+    }
+})
